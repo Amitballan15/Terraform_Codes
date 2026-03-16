9308916018ccf5437502f9961a507f6022476bd5
Amit Ballan <amit.ballan@agreeya.net>
2026-03-16 16:41:40 +0530

    basic stg added

--- resource-group-basic-code/main.tf
+++ resource-group-basic-code/main.tf
@@ -18,10 +18,10 @@ resource "azurerm_resource_group" "test" {
 
 }
 
-resource "azurerm_storage_account" "teststg" {
-    name = "demowalastgforballan"
-    resource_group_name = azurerm_resource_group.test
-    location = "westus"
-
-  
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
